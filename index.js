0064f4c4c538b292082c7836530593c9a2ed96cd
SanzarRehman <47343673+SanzarRehman@users.noreply.github.com>
2021-07-29 19:00:16 +0600

    final

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ const schema = Joi.object({
 const result =schema.validate(req.body);
 console.log(result);
 
+if(result.error)
+{
+
+    res.status(400).send(result.error.details[0].message);
+    return;
+}
+
 const course ={
 id:courses.length+1,
 name: req.body.name
@@ -36,10 +43,52 @@ res.send(course);
 
 
 });
+
+
+app.put('/api/courses/:id',(req,res)=>{
+    const course= courses.find(c=>c.id== parseInt(req.params.id))
+
+    if(!course) res.status(404).send('The course has not found');
+   
+
+    
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    }); 
+    const result =schema.validate(req.body);
+    console.log(result);
+
+    if(result.error)
+{
+
+    res.status(400).send(result.error.details[0].message);
+    return;
+}
+
+
+course.name=req.body.name;
+res.send(course);
+
+
+
+
+
+});
 /////////////////////////////////
 
+
+/////////////////////
+
+
+
+
+
+
+
+////////////
+
     app.get('/api/courses',(req,res) =>{
-        res.send([1,2,3]);
+        res.send(courses);
 
     });
     app.get('/api/course/:id',(req,res) =>
