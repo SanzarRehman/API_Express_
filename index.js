454c85b2802e880033362e2960b2328dc031b9ad
SanzarRehman <47343673+SanzarRehman@users.noreply.github.com>
2021-07-29 15:24:17 +0600

    post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
+const Joi = require('joi');
 const express =require('express');
 const app = express();
+app.use (express.json());
 const courses =[
 {id:1,name:'course 1'},
 {id:2,name:'course 2'},
@@ -14,6 +15,23 @@ app.get('/',(req,res) =>{
     res.send('Hello World!!');
 
 });
+
+/////////////////////////////////////
+app.post('/api/courses',(req,res)=>{
+
+
+    
+const course ={
+id:courses.length+1,
+name: req.body.name
+
+
+};
+courses.push(course);
+
+
+});
+/////////////////////////////////
 
     app.get('/api/courses',(req,res) =>{
         res.send([1,2,3]);
